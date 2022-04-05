c90f52dd388a25738eaa555846eed1b60630c700
ivoryttang <ivoryttang.com>
2022-04-05 00:22:43 -0400

    planning algorithm framework

--- src/path_planning.py
+++ src/path_planning.py
@@ -19,6 +19,8 @@ class PathPlan(object):
         self.trajectory = LineTrajectory("/planned_trajectory")
         self.goal_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_cb, queue_size=10)
         self.traj_pub = rospy.Publisher("/trajectory/current", PoseArray, queue_size=10)
+        self.start_sub = rospy.Subscriber("/initialpose", PoseStamped, self.start_cb, queue_size=10)
+        self.clicked_sub = rospy.Subscriber("/clicked_point", PoseStampled, self.clicked_cb, queue_size=10)
         self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_cb)
 
         # Map traits
@@ -27,8 +29,19 @@ class PathPlan(object):
         self.map_set = False
         self.map_resolution = None
 
+        # odom traits
+
+        # Start Traits
+        self.start = None
+
+        # clicked traits
+        self.clicked = None
+        
         # Goal Traits
         self.goal = None
+
+        #publish trajectory
+        self.plan_path(self.start, self.goal, self.map)
 
 
     def map_cb(self, msg):
@@ -99,9 +112,78 @@ class PathPlan(object):
 
         return None
 
+    def start_cb(self, msg):
+        '''
+        Stores the start location as a 4x4 Transformation matrix
+        '''
+        self.start = quaternion_matrix([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        self.start[0,3] = msg.pose.position.x
+        self.start[1,3] = msg.pose.position.y
+        self.start[2,3] = 0
+
+        return None
+
+    def clicked_cb(self, msg):
+        '''
+        Stores the clicked location as a 4x4 Transformation matrix
+        '''
+        self.clicked = quaternion_matrix([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        self.clicked[0,3] = msg.pose.position.x
+        self.clicked[1,3] = msg.pose.position.y
+        self.clicked[2,3] = 0
+
+        return None
+
+    def heuristic(self, node1, node2):
+        #TODO: implement dubins curve solution (used in plan_path)
+        pass
+        
     def plan_path(self, start_point, end_point, map):
         ## CODE FOR PATH PLANNING ##
 
+        # A* search
+        frontier = PriorityQueue()
+        start_u, start_v = self.xy_to_uv(start_point)
+        end_u, end_v = self.xy_to_uv(start_point)
+        frontier.put((start_v, start_u), 0)
+        came_from = dict()
+        cost_so_far = dict()
+        came_from[(start_v, start_u)] = None
+        cost_so_far[(start_v, start_u)] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == end_point:
+                break
+
+            u, v = current[0], current[1]
+            neighbors = {(v, u+1), (v, u-1), (v+1, u), (v-1, u), (v-1,u-1), (v-1, u+1), (v+1, u-1), (v+1, u+1)}
+            for next in neighbors:   #graph.neighbors(current)
+                # TODO: ignore this path if obstacle
+                
+                new_cost = cost_so_far[current] + 1  #TODO: 1 is placeholder for graph.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic((end_v, end_u), next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+                    
+        # set up trajectory based on A* search results
+        s = Point()
+        curr = (start_v, start_u)
+        s.x, s.y = self.uv_to_xy((start_v, start_u))[0], self.uv_to_xy((start_v, start_u))[1]
+        self.trajectory.points += [x]
+        self.trajectory.distances += [0]
+        while curr in came_from.keys():
+            next = came_from[curr]
+            s = Point()
+            s.x, s.y = self.uv_to_xy(next)[0], self.uv_to_xy(next)[1]
+            self.trajectory.points += [x]
+            self.trajectory.distances += cost_so_far[curr]
+            curr = next
+         
+        
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
 
