a60ec520260397b7bdd1dbc967a02dda2c3d99a4
LucianCov <lucianc@mit.edu>
2022-04-09 23:36:06 +0000

    Made functional planner

--- src/path_planning.py
+++ src/path_planning.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
+from audioop import reverse
 import rospy
 import numpy as np
-from geometry_msgs.msg import PoseStamped, PoseArray, Point
+from geometry_msgs.msg import PoseStamped, PoseArray, Point, PointStamped, PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry, OccupancyGrid
 from tf.transformations import quaternion_from_euler, euler_from_quaternion, quaternion_matrix
-from skimage.morphology import dilation, disk
-import queue
-import dubins
-import rospkg
+from scipy.signal import convolve2d
+from Queue import PriorityQueue
+# import rospkg
 import time, os
 from utils import LineTrajectory
 
@@ -22,8 +22,8 @@ class PathPlan(object):
         self.trajectory = LineTrajectory("/planned_trajectory")
         self.goal_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_cb, queue_size=10)
         self.traj_pub = rospy.Publisher("/trajectory/current", PoseArray, queue_size=10)
-        self.start_sub = rospy.Subscriber("/initialpose", PoseStamped, self.start_cb, queue_size=10)
-        self.clicked_sub = rospy.Subscriber("/clicked_point", PoseStampled, self.clicked_cb, queue_size=10)
+        self.start_sub = rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.start_cb, queue_size=10)
+        self.clicked_sub = rospy.Subscriber("/clicked_point", PointStamped, self.clicked_cb, queue_size=10)
         self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_cb)
 
         # Map traits
@@ -31,6 +31,10 @@ class PathPlan(object):
         self.map_Transform = None
         self.map_set = False
         self.map_resolution = None
+
+        # For eroding the map with scipy
+        self.mask = np.ones((15,15))
+        self.stride = 2
 
         # odom traits
         self.turning_radius = 1
@@ -47,7 +51,7 @@ class PathPlan(object):
         self.goal_theta = None
 
         #publish trajectory
-        self.plan_path(self.start, self.goal, self.map)
+        # self.plan_path(self.start, self.goal, self.map)
 
 
     def map_cb(self, msg):
@@ -55,14 +59,20 @@ class PathPlan(object):
         Populates our map fields in the class with the map object, and sets the 
         map_set param to true so that other functions can run
         '''
+        rospy.loginfo("setting map")
         # data is indexed by (u,v) now that it is reshaped and transposed
         map = np.array(msg.data).reshape((msg.info.height, msg.info.width)).T
-        self.map = np.zeros((msg.info.width, msg.info.height))
+
         # make all nonzero values 1 (including -1 vals)
         map[np.nonzero(map)] = 1
-        # Now the map is dilated by disks of 8px radius
-        dilation(self.map, disk(8), out=self.map)
+
+        # Now the map is dilated(sorta) by a square of size 7
+        self.map = convolve2d(map, self.mask, mode="same")
         self.map = self.map.astype(int)
+
+        # NOTE: Can uncomment the stride portion to downsample the map in order to increase
+        # speed. Will reduce accuracy though
+        self.map = self.map[0:self.map.shape[0]:self.stride, 0:self.map.shape[1]:self.stride]
 
         self.map_resolution = msg.info.resolution
 
@@ -81,13 +91,15 @@ class PathPlan(object):
         Converts from (x,y) -> (u,v)
         '''
         if not self.map_set:
+            rospy.loginfo("Map not set yet")
             return
 
-        xy_homo = np.array([coord[0],coord[1],0,1]).T
+        xy_homo = np.array([[coord[0],coord[1],0,1]]).T
 
         uv_homo = np.matmul(np.linalg.inv(self.map_Transform), xy_homo)
+        # rospy.loginfo(uv_homo.shape)
 
-        return uv_homo[0,0]/self.map_resolution , uv_homo[1,0]/self.map_resolution
+        return int(np.round(uv_homo[0,0]/self.map_resolution)) , int(np.round(uv_homo[1,0]/self.map_resolution))
 
     def uv_to_xy(self, coord):
         '''
@@ -101,7 +113,7 @@ class PathPlan(object):
         if not self.map_set:
             return
 
-        uv_homo = np.array([coord[0],coord[1],0,1]).T * self.map_resolution
+        uv_homo = np.array([[coord[0],coord[1],0,1]]).T * self.map_resolution
         uv_homo[3,0] = 1
 
         xy_homo = np.matmul(self.map_Transform, uv_homo)
@@ -117,11 +129,15 @@ class PathPlan(object):
         '''
         Stores the goal location as a 4x4 Transformation matrix
         '''
-        self.goal = quaternion_matrix([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-        self.goal[0,3] = msg.pose.position.x
-        self.goal[1,3] = msg.pose.position.y
-        self.goal[2,3] = 0
-        self.goal_theta = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])[2]
+        # self.goal = quaternion_matrix([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        # self.goal[0,3] = msg.pose.position.x
+        # self.goal[1,3] = msg.pose.position.y
+        # self.goal[2,3] = 0
+        # self.goal_theta = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])[2]
+        self.goal = int(np.round(msg.pose.position.x)), int(np.round(msg.pose.position.y))
+        if self.start is not None and self.map_set:
+            self.trajectory.clear()
+            self.plan_path()
 
         return None
 
@@ -129,11 +145,15 @@ class PathPlan(object):
         '''
         Stores the start location as a 4x4 Transformation matrix
         '''
-        self.start = quaternion_matrix([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-        self.start[0,3] = msg.pose.position.x
-        self.start[1,3] = msg.pose.position.y
-        self.start[2,3] = 0
-        self.start_theta = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])[2]
+        # self.start = quaternion_matrix([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        # self.start[0,3] = msg.pose.pose.position.x
+        # self.start[1,3] = msg.pose.pose.position.y
+        # self.start[2,3] = 0
+        # self.start_theta = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])[2]
+        self.start = int(np.round(msg.pose.pose.position.x)), int(np.round(msg.pose.pose.position.y))
+        # if self.goal is not None and self.map_set:
+        #     self.trajectory.clear()
+        #     self.plan_path()
 
         return None
 
@@ -150,57 +170,70 @@ class PathPlan(object):
 
     def heuristic(self, node1, node2):
         # Dubins curve heuristic (used in plan_path)
-        start = node1+[0] #TODO: Change [0] to an angle that matches the path better
-        end = node2+[self.goal_theta]
-        return dubins.path_length(dubins.shortest_path(start, end, self.turning_radius))
+        # start = node1+[0] #TODO: Change [0] to an angle that matches the path better
+        # end = node2+[self.goal_theta]
+        # return dubins.path_length(dubins.shortest_path(start, end, self.turning_radius))
+
+        # Euclidian Dist
+        return ((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)**.5
         
-    def plan_path(self, start_point, end_point, map):
+    def plan_path(self):
         ## CODE FOR PATH PLANNING ##
 
         # A* search
-        frontier = queue.PriorityQueue()
-        start_u, start_v = self.xy_to_uv(start_point)
-        end_u, end_v = self.xy_to_uv(end_point)
-        frontier.put((start_v, start_u), 0)
+        rospy.loginfo("Planning Path")
+        frontier = PriorityQueue()
+        start_u, start_v = self.xy_to_uv(self.start)
+        end_u, end_v = self.xy_to_uv(self.goal)
+        frontier.put((0,0,(start_u, start_v)))
+
         came_from = dict()
         cost_so_far = dict()
-        came_from[(start_v, start_u)] = None
-        cost_so_far[(start_v, start_u)] = 0
+
+        # came_from[(start_u, start_v)] = None
+        cost_so_far[(start_u, start_v)] = 0
 
         while not frontier.empty():
-            current = frontier.get()
+            current = frontier.get()[2]
 
-            if current == end_point:
+            if current == self.goal:
                 break
 
             u, v = current[0], current[1]
             neighbors = {(u+1, v), (u-1, v), (u, v+1), (u, v-1), (u-1,v-1), (u-1, v+1), (u+1, v-1), (u+1, v+1)}
             for next in neighbors:   #graph.neighbors(current)
                 # Ignore this path if obstacle
-                if self.map[next[0], next[1]] == 1:
+                try:
+                    if self.map[int(next[0]/self.stride), int(next[1]/self.stride)] != 0:
+                        continue
+                except(e):
+                    # if out of bounds
+                    print "error: ", e
                     continue
                 
-                new_cost = cost_so_far[current] + 1  #TODO: 1 is placeholder for graph.cost(current, next)
+                new_cost = cost_so_far[current] + self.heuristic(current, next)
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + self.heuristic(next,(end_u, end_v))
-                    frontier.put(next, priority)
+                    frontier.put((priority, time.time(), next))
                     came_from[next] = current
                     
         # set up trajectory based on A* search results
         s = Point()
-        curr = (start_v, start_u)
-        s.x, s.y = self.uv_to_xy((start_v, start_u))[0], self.uv_to_xy((start_v, start_u))[1]
-        self.trajectory.points += [(s.x, s.y)]
-        self.trajectory.distances += [0]
+        curr = (end_u, end_v)
+        s.x, s.y = self.uv_to_xy((end_u, end_v))
+        pts = [s]
+        # self.trajectory.addPoint(s)
         while curr in came_from.keys():
             next = came_from[curr]
             s2 = Point()
-            s2.x, s2.y = self.uv_to_xy(next)[0], self.uv_to_xy(next)[1]
-            self.trajectory.points += [(s2.x, s2.y)]
-            self.trajectory.distances += cost_so_far[curr]
+            s2.x, s2.y = self.uv_to_xy(next)
+            # self.trajectory.addPoint(s2)
+            pts.append(s2)
             curr = next
-         
+        
+        for e in reversed(pts):
+            self.trajectory.addPoint(e)
         
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
