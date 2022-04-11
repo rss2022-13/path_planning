quadratic formula bug testing

src/pure_pursuit.py
@@ -16,8 +16,8 @@ class PurePursuit(object):
     """
     def __init__(self):
         self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = 1 # Tune later
-        self.speed            = 1 # Tune later
+        self.lookahead        = 0.5 # Tune later
+        self.speed            = 0.5 # Tune later
         self.wheelbase_length = 0.5 # Measure later
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
@@ -43,15 +43,21 @@ class PurePursuit(object):
             closest: Tuple of the closest point, closest distance, and the index value of the trajectory 
                     segment (starting from 0)
         """
+        if len(self.trajectory.points) < 0:
+            return None
         # Current position based on odometry
         cur_pos = np.array([odom.pose.pose.position.x, odom.pose.pose.position.y])
 
         # Trajectory points
-        traj_pts = np.array(self.trajectory.points)
-        
+        traj_pts = np.zeros((len(self.trajectory.points), 2))
+        for i in range(len(self.trajectory.points)):
+            point = self.trajectory.points[i]
+            traj_pts[i][0] = point[0]
+            traj_pts[i][1] = point[1]
+
         # Relative Trajectory Segment Vectors
         traj_deltas = np.diff(traj_pts, axis=0)
-        traj_norms = np.linalg.norm(traj_deltas, axis=1)
+        traj_norms = np.sqrt(np.sum(np.multiply(traj_deltas, traj_deltas), axis=1))
 
         # Relative Vectors from Current Pose to Trajectory Points
         traj2pos_deltas = np.subtract(cur_pos, traj_pts[:-1])
@@ -66,10 +72,17 @@ class PurePursuit(object):
             # Multiply this value to the segment deltas and add it to the traj_pts to get closest points
             # on each segment
         closest_pts_list = np.add(traj_pts[:-1], np.multiply(traj_deltas, capped.reshape(len(capped), 1)))
-        closest_dist_list = np.linalg.norm(np.subtract(cur_pos, closest_pts), axis=1)
+        difference = np.subtract(cur_pos, closest_pts_list)
+        closest_dist_list = np.sqrt(np.sum(np.multiply(difference,difference), axis=1))
         # index of the actual closesst point
-        c_ind = np.argmin(closest_dist_list)
-
+        if len(closest_dist_list) > 0:
+            c_ind = closest_dist_list.argmin()
+        else:
+            rospy.loginfo("Something went wrong again")
+            rospy.loginfo(len(closest_pts_list))
+            rospy.loginfo(len(traj_deltas))
+            return None
+        rospy.loginfo(len(closest_dist_list))
         # Return a tuple of the closest distance, closest point, and the index value
         closest = (closest_pts_list[c_ind][:], closest_dist_list[c_ind], c_ind)
         return closest
@@ -80,80 +93,94 @@ class PurePursuit(object):
         
         cur_pos = np.array([odom.pose.pose.position.x, odom.pose.pose.position.y])
         closest = self.find_closest_point(odom)
+        if closest is None:
+            return None
 
-        traj_pts = np.array(self.trajectory.points)
+        traj_pts = np.zeros((len(self.trajectory.points), 2))
+        for i in range(len(self.trajectory.points)):
+            point = self.trajectory.points[i]
+            traj_pts[i][0] = point[0]
+            traj_pts[i][1] = point[1]
 
         closest_pt = closest[0]
         closest_dist = closest[1]
         closest_ind = closest[2]
 
 
-        if (closest_dist >= self.lookahead):
+        if (abs(closest_dist-self.lookahead) < 1e-6):
             return closest_pt
         else:
-            # Iterator for goal point search
-            i = 1
-
-            while i+closest_ind < len(traj_pts):
-                # Trajectory Segment (starting at the closest point)
-                start_pt = closest_pt if i == 1 else traj_pts[closest_ind+i-1]
-                
-                end_pt = traj_pts[closest_ind+i]
-                seg_delta = np.subtract(end_pt, start_pt)
-
-                robot_to_start = np.subtract(start_pt, cur_pos)
-                # Guaranteed intersection with lookahead circle if the closest distance is 
-                # less than lookahead distance
-                a = np.dot(seg_delta,seg_delta)
-                b = 2*np.dot(seg_delta, robot_to_start)
-                c = np.dot(robot_to_start, robot_to_start) - self.lookahead**2
-
-                # Since we are starting at the closest point
-                # and we are assuming it is within the circle
-                # the correct t is positive while the other is negative
-                t1 = (-b+np.sqrt(b**2 - 4*a*c))/(2*a)
-                t2 = (-b-np.sqrt(b**2 - 4*a*c))/(2*a)
+            # Find the first point that is farther than the lookahead distance
+            first_out = closest_ind
+            for i in range(closest_ind+1, len(traj_pts)):
+                pt = traj_pts[i][:]
+                squared_dist = np.dot(cur_pos-pt)
+                if (squared_dist > self.lookahead**2):
+                    first_out = i
+                    break
+            out_pt = traj_pts[first_out][:]
+            in_pt = traj_pts[first_out-1][:]
             
+            vec_v = out_pt - in_pt;
+            vec_w = in_pt - cur_pos;
+            
+            c = dot(vec_w, vec_w) - self.lookahead**2
+            b = 2*dot(vec_v, vec_w)
+            a = dot(vec_v, vec_v)
+            
+            t = 0
+            if (b**2 - 4*a*c) > 0:
+                t1 = (-b + np.sqrt(b**2 - 4*a*c))/(2*a)
+                t2 = (-b - np.sqrt(b**2 - 4*a*c))/(2*a)
                 t = max(t1, t2)
-                if t < 0:
-                    print("Something went very wrong")
-                elif t < 1:
-                    return np.add(start_pt, np.multiply(seg_delta, t))
-                else:
-                    i += 1
-            return None
+
+                return in_pt + t*vec_v
+
+            elif abs(b**2 - 4*a*c) < 1e-6 and b < 0:
+                t = -b/(2*a)
+
+                return in_pt + t*vec_v
+
+            else:
+                rospy.loginfo("wtf")
+
+                return None
             
             
 
     def pursuit(self, msg):
         """ Publishes drive instructions based on current PF pose information
         """
-       # Find the goal point
-       goal_point = self.find_goal_point(msg)
-
-       # Find current position and orientation
-       cur_pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, 0])
-       cur_theta = np.arccos(msg.pose.pose.orientation.w)*2
+        # Find the goal point
+        goal_point = self.find_goal_point(msg)
+        if goal_point is not None:
+            goal_point = np.append(goal_point, [0])
+            # Find current position and orientation
+            cur_pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, 0])
+            cur_theta = np.arccos(msg.pose.pose.orientation.w)*2
        
-       # Determine the perpendicular distance to the goal point from the car position
-       orientation_vec = np.array([np.cos(cur_theta), np.sin(cur_theta), 0])
-       diff_vec = np.subtract(goal_point, cur_pos)
-       cross = np.cross(diff_vec, orientation_vec)
-       perp_dist = cross[2] # Allowed to be negative to distinguish between turing left or right
+            # Determine the perpendicular distance to the goal point from the car position
+            orientation_vec = np.array([np.cos(cur_theta), np.sin(cur_theta), 0])
+            diff_vec = np.subtract(goal_point, cur_pos)
+            cross = np.cross(orientation_vec, diff_vec)
+            perp_dist = cross[2] # Allowed to be negative to distinguish between turing left or right
        
-       arc_curvature = (2*perp_dist)/(self.lookahead**2)
-       steering_angle = np.arctan(self.wheelbase_length*arc_curvature) # Getting steering angle from curvature
+            arc_curvature = (2*perp_dist)/(self.lookahead**2)
+            steering_angle = np.arctan(self.wheelbase_length*arc_curvature) # Getting steering angle from curvature
+            
+            ack_msg = AckermannDriveStamped()
+            ack_msg.header.stamp = rospy.Time.now()
+            ack_msg.header.frame_id = msg.header.frame_id
+            ack_msg.drive.steering_angle = steering_angle
+            ack_msg.drive.speed = self.speed
 
-       ack_msg = AckermannDriveStamped()
-       ack_msg.header.stamp = rospy.Time.now()
-       ack_msg.header.frame_id = msg.header.frame_id
-       ack_msg.drive.steering_angle = steering_angle
-       ack_msg.drive.speed = self.speed
-
-       self.drive_pub.publish(ack_msg)
+            self.drive_pub.publish(ack_msg)
+            rospy.loginfo("Is this working")
+        else:
+            rospy.loginfo("Not working, empty trajectory")
 
 
 if __name__=="__main__":
     rospy.init_node("pure_pursuit")
     pf = PurePursuit()
-    rosy.spin()
+    rospy.spin()